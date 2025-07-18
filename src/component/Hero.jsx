58419ebce41208b834cd7f3ed9aee17bcf66a462
Zafar Ahmad <za6182366@gmail.com>
2025-07-18 18:01:35 +0530

    delete

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div style={{backgroundImageurl:`/src/assets/heroImage.png`}} className='flex flex-col items-start justify-center px-6 
     md:px- lg:px-24 xl:px-32 text-white  bg-no-repeat bg-cover h-screen'>
-        {/* zafar gandu hai */}
+
       
     </div>
   )
