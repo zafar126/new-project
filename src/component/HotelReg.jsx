ff2a9e522a1aebf1d2bfe8168fd52135ad92ac26
Zafar Ahmad <za6182366@gmail.com>
2025-07-30 21:54:49 +0530

    HotelRegGoingOn

--- src/component/HotelReg.jsx
+++ src/component/HotelReg.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const HotelReg = () => {
   return (
-    <div className='fixed top-0 bottom-0 '>
+    <div className='fixed top-0 bottom-0 left-0 right-0 z-100 flex items-center justify-center bg-black/75'>
+        <form>
+            <img src={assets.tegImage} alt="reg-image" className='w-1/2 rounded-xl hiddenmd:clock' />
+        </form>
       
     </div>
   )
