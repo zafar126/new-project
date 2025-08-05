2cae4afdcbbad9c0c80977e1d2477c2e1f7a429f
Zafar Ahmad <za6182366@gmail.com>
2025-08-05 21:57:12 +0530

    dash

--- src/pages/hotelOwner/Layout.jsx
+++ src/pages/hotelOwner/Layout.jsx
@@ -10,7 +10,7 @@ const Layout = () => {
         <Navbar />
        <div className='flex h-full'>
          <Sidebar />
-         
+        
          <div className='flex-1 p-4 pt-10 md:px-10 h-full'>
             <Outlet/>
          </div>
